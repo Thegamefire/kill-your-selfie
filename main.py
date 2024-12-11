9887edd7f626ffa5ada2c82074211247c5165748
Thegamefire <Thegamefire@outlook.com>
2024-12-11 15:52:05 +0100

    Added template

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask
+import psycopg2
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+load_dotenv()
+db_username = os.environ.get("USER_NAME")
+db_password = os.environ.get("PASSWORD")
+db_host = os.environ.get("HOST")
+db_port = os.environ.get("PORT")
+db_database = os.environ.get('DATABASE')
+
 @app.route("/")
 def homepage():
-    return "<h1>Go Kill your Selfie!</h1>"
-
+    return render_template('index.html')
 
 if __name__ == '__main__':  
    app.run()
